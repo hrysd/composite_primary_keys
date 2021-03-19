1e037dd9a16b84596268907fb885af0068211dbc
Hiroshi Yoshida <hrysd22@gmail.com>
2021-03-19 18:54:28 +0900

    Do not overwrite `ActiveRecord::Calculations#build_count_subquery`.
    Array is not passed to `build_count_subquery` as `column_name`.
    So There are no composite_primary_keys specific implementations

--- lib/composite_primary_keys/relation/calculations.rb
+++ lib/composite_primary_keys/relation/calculations.rb
@@ -1,28 +1,6 @@
 module CompositePrimaryKeys
   module ActiveRecord
     module Calculations
-      def build_count_subquery(relation, column_name, distinct)
-        if column_name == :all
-          column_alias = Arel.star
-          # CPK
-          # relation.select_values = [ Arel.sql(FinderMethods::ONE_AS_ONE) ] unless distinct
-          relation.select_values = [ Arel.sql(::ActiveRecord::FinderMethods::ONE_AS_ONE) ] unless distinct
-        elsif column_name.is_a?(Array)
-          column_alias = Arel.star
-          relation.select_values = column_name.map do |column|
-            Arel::Attribute.new(@klass.unscoped.table, column)
-          end
-        else
-          column_alias = Arel.sql("count_column")
-          relation.select_values = [ aggregate_column(column_name).as(column_alias) ]
-        end
-
-        subquery_alias = Arel.sql("subquery_for_count")
-        select_value = operation_over_aggregate_column(column_alias, "count", false)
-
-        relation.build_subquery(subquery_alias, select_value)
-      end
-
       def calculate(operation, column_name)
         if has_include?(column_name)
           relation = apply_join_dependency
