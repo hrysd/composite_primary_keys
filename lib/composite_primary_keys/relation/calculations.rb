dda3574b0ecb2e703ee1f8d36dd4f139762fc719
Hiroshi Yoshida <hrysd22@gmail.com>
2021-03-22 14:44:56 +0900

    Overwrite `ActiveRecord::Calculations::calculate` to generate COUNT query without subquery.
    When operation like followings, CPK generates subquery always.
    And I don't think `ActiveRecord::Calculations#execute_simple_calculation` needs CPK specific implementation.
    So stop to overwrite this.
    
    ```
    User.count(:id)
    ```
    
    ```
    SELECT COUNT(count_column) FROM (SELECT `users`.`id` AS count_column FROM `users`) subquery_for_count
    ```
    
    But if just remove `execute_simple_calculations`, the query could not resolve primary keys when joining table.
    
    ```
    User.references(:articles).count
    
    ```
    
    because model using CPK returns array of primary keys and SELECT value is like this.
    It make select values ambiguous when joining table has same column name
    
    ```sql
    class User < AR::Base
      self.primary_keys = :user_id, :foo_id
    end
    
    class Article < AR::Base
      self.primary_keys = :user_id, :article_id
    end
    ```
    
    ```SQL
    SELECT COUNT(DISTINCT user_id, created_at) FROM ....
    ```
    
    So I overwrite `calculate` instead of `execute_simple_calculations` like current master
    to generate COUNT query without subquery

--- lib/composite_primary_keys/relation/calculations.rb
+++ lib/composite_primary_keys/relation/calculations.rb
@@ -14,46 +14,6 @@ module CompositePrimaryKeys
         end
       end
 
-      def execute_simple_calculation(operation, column_name, distinct) #:nodoc:
-        column_alias = column_name
-
-        # CPK
-        # if operation == "count" && (column_name == :all && distinct || has_limit_or_offset?)
-        #   # Shortcut when limit is zero.
-        #   return 0 if limit_value == 0
-        #
-        #   query_builder = build_count_subquery(spawn, column_name, distinct)
-        if operation == "count"
-          relation = unscope(:order)
-          query_builder = build_count_subquery(spawn, column_name, distinct)
-        else
-          # PostgreSQL doesn't like ORDER BY when there are no GROUP BY
-          relation = unscope(:order).distinct!(false)
-
-          column = aggregate_column(column_name)
-
-          select_value = operation_over_aggregate_column(column, operation, distinct)
-          if operation == "sum" && distinct
-            select_value.distinct = true
-          end
-
-          column_alias = select_value.alias
-          column_alias ||= @klass.connection.column_name_for_operation(operation, select_value)
-          relation.select_values = [select_value]
-
-          query_builder = relation.arel
-        end
-
-        result = skip_query_cache_if_necessary { @klass.connection.select_all(query_builder, nil) }
-        row    = result.first
-        value  = row && row.values.first
-        type   = result.column_types.fetch(column_alias) do
-          type_for(column_name)
-        end
-
-        type_cast_calculated_value(value, type, operation)
-      end
-
       def build_count_subquery(relation, column_name, distinct)
         if column_name == :all
           relation.select_values = [ Arel.sql(::ActiveRecord::FinderMethods::ONE_AS_ONE) ] unless distinct
@@ -75,6 +35,34 @@ module CompositePrimaryKeys
         select_value = operation_over_aggregate_column(column_alias || Arel.star, "count", false)
 
         Arel::SelectManager.new(subquery).project(select_value)
+      end
+
+      def calculate(operation, column_name)
+        if has_include?(column_name)
+          relation = apply_join_dependency
+
+          if operation.to_s.downcase == "count"
+            unless distinct_value || distinct_select?(column_name || select_for_count)
+              relation.distinct!
+
+              # CPK
+              # relation.select_values = [ klass.primary_key || table[Arel.star] ]
+              if klass.primary_key.present? && klass.primary_key.is_a?(Array)
+                relation.select_values = klass.primary_key.map do |k|
+                  "#{connection.quote_table_name(klass.table_name)}.#{connection.quote_column_name(k)}"
+                end
+              else
+                relation.select_values = [ klass.primary_key || table[Arel.star] ]
+              end
+            end
+            # PostgreSQL: ORDER BY expressions must appear in SELECT list when using DISTINCT
+            relation.order_values = []
+          end
+
+          relation.calculate(operation, column_name)
+        else
+          perform_calculation(operation, column_name)
+        end
       end
     end
   end
